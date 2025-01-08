c5965d3a41d1fd51057ea7071d8dd224f83db6da
Paweł Kaźmierczak <102613700+PawelKazmierczak1998@users.noreply.github.com>
2025-01-08 19:38:09 +0100

    Changed Employee model, added ability to add/update/Delete Employee , see Details of Employee

--- EmployeeManagementSystem/BaseLibrary/Entities/Employee.cs
+++ EmployeeManagementSystem/BaseLibrary/Entities/Employee.cs
@@ -1,28 +1,29 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace BaseLibrary.Entities
 {
     public class Employee : BaseEntity
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? CivilId { get; set; }
-        public string? FileNumber { get; set; }
-        public string? FullName { get; set; }
-        public string? JobName { get; set; }
-        public string? Address { get; set;}
-        public string? TelephoneNumber { get; set;}
-        public string? Photo {  get; set;}
-        public string? Other { get; set;}
+        [Required]
+        public string CivilId { get; set; }
+        [Required]
+        public string FileNumber { get; set; }
+        [Required]
+        public string JobName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required, DataType(DataType.PhoneNumber)]
+        public string TelephoneNumber { get; set; }
+        [Required]
+        public string Photo {  get; set; } =string.Empty;
+        public string? Other { get; set; }
 
-        //Relacje Wielu do jednego
-
-        public GeneralDepartment? GeneralDepartment { get; set;}
-        public int GeneralDepartmentId { get; set; }
-        public Department? Department { get; set; }
-        public int DepartmentId { get; set; }
+        //Relacje Wielu do jednego z Branch
 
         public Branch? Branch { get; set; }
         public int BranchId { get; set; }
+
         public Town? Town { get; set; }
         public int TownId { get; set; }
     }
